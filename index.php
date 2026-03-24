9b37fae34b14f9967fa6b9e28b17927a087db6a4
Masagus Ahmad Ramadhan <agusyantosugiyanto@gmail.com>
2026-03-24 11:55:20 +0700

    create local server

--- index.php
+++ index.php
@@ -8,16 +8,13 @@ $mainresult = mysqli_query($conn, $mainquery);
 $userdata = mysqli_fetch_assoc($mainresult);
 
 $incomequery = "SELECT * FROM income ORDER BY id DESC";
-$incomeresult = mysqli_query($conn, $incomequery);
-$income = mysqli_fetch_assoc($incomeresult);
+$income = mysqli_query($conn, $incomequery);
 
 $outcomequery = "SELECT * FROM outcome ORDER BY id DESC";
-$outcomeresult = mysqli_query($conn, $outcomequery);
-$outcome = mysqli_fetch_assoc($outcomeresult);
+$outcome = mysqli_query($conn, $outcomequery);
 
 $constquery = "SELECT * FROM const ORDER BY id DESC";
-$constresult = mysqli_query($conn, $constquery);
-$const = mysqli_fetch_assoc($constresult);
+$const = mysqli_query($conn, $constquery);
 ?>
 
 <head>
@@ -28,30 +25,54 @@ $const = mysqli_fetch_assoc($constresult);
 </head>
 
 <body>
-    <div id="currency">
-        <span>Rp. <b id="currency__total">
-                <?= number_format($userdata['currency'], 0, ',', '.'), ",00" ?>
-            </b></span>
+    <div id="currency" class="cards">
+        <span>Rp. <b id="currency__total"></b></span>
         <p>Your Current Money</p>
     </div>
-    <div id="name">
-        <h1>
-            <?= htmlspecialchars($userdata['name']) ?>`s
-        </h1>
-        <span>Wallet</span>
+    <div id="name" class="cards">
+        <h1 id="username"></h1>
+        <span>Wallets</span>
     </div>
-    <div id="income">
-
+    <div id="income" class="cards">
+        <?php while ($income_log = mysqli_fetch_array($income)): ?>
+            <div class="incomes" title="<?= $income_log['date'] ?>">
+                <h2><?= htmlspecialchars($income_log['detail']) ?></h2>
+                <span>+<?= number_format($income_log['income'], 0, ',', '.') ?></span>
+            </div>
+        <?php endwhile ?>
     </div>
-    <div id="outcome"></div>
+    <div id="outcome" class="cards">
+        <?php while ($outcome_log = mysqli_fetch_array($outcome)): ?>
+            <div class="outcomes" title="<?= $outcome_log['date'] ?>">
+                <h2><?= htmlspecialchars($outcome_log['detail']) ?></h2>
+                <span>+<?= number_format($outcome_log['outcome'], 0, ',', '.') ?></span>
+            </div>
+        <?php endwhile ?>
+    </div>
 
-    <div id="calc" title="Calculator" onclick="goToApp('calc-app')">
+    <div id="calc" title="Calculator" onclick="goToApp('calc-app')" class="cards">
         <img src="src/img/calc.svg">
     </div>
 
-    <div id="convert" title="Converter" onclick="goToApp('convert-app')">
+    <div id="convert" title="Converter" onclick="goToApp('convert-app')" class="cards">
         <img src=" src/img/convert.svg">
     </div>
+
+    <div id="estimate-control" class="cards"></div>
+
+    <div id="estimate" class="cards"></div>
+
+    <div id="add-income" class="cards">
+        <img src="src/img/up.svg">
+        <p>Add Income</p>
+    </div>
+
+    <div id="add-outcome" class="cards">
+        <p>Add Outcome</p>
+        <img src="src/img/down.svg">
+    </div>
+
+    <div id="const" class="cards"></div>
 
     <script src="src/script/main.js"></script>
 </body>
--- src/script/main.js
+++ src/script/main.js
@@ -1,19 +1,44 @@
-const current_money = document.getElementById("currency__total")
-var money = current_money.textContent
-console.log(parseInt(money) * 1000)
+const money_display = document.getElementById('currency__total')
+const username = document.getElementById('username')
 
-if (parseInt(money) > 0) {
-    current_money.classList.add("plus");
-    current_money.classList.remove("minus");
-} else if (parseInt(money) < 0) {
-    current_money.classList.add("minus");
-    current_money.classList.remove("plus");
-} else {
-    current_money.classList.remove("plus", "minus");
+var data_list;
+
+var name;
+var money;
+
+async function ambilDataDariServer() {
+    try {
+        // Meminta data ke server Node.js
+        const response = await fetch('http://localhost:3000/api/currencies');
+        const data = await response.json();
+        data_list = data;
+        
+        name = data_list[0].name
+        money = data_list[0].currency
+        
+        currency_format = new Intl.NumberFormat("id-ID").format(money)
+        console.log(name, ": ", currency_format, " ", typeof(currency_format))
+
+        money_display.textContent = currency_format + ",00";
+        username.textContent = name
+
+        if (parseInt(money) > 0) {
+            money_display.classList.add("plus");
+            money_display.classList.remove("minus");
+        } else if (parseInt(money) < 0) {
+            money_display.classList.add("minus");
+            money_display.classList.remove("plus");
+        } else {
+            money_display.classList.remove("plus", "minus");
+        }
+
+    } catch (error) {
+        console.error("Server belum jalan atau error:", error);
+    }
 }
+ambilDataDariServer();
 
 function goToApp(app) {
     target = "apps/"+app+".html";
     window.location.href += target;
-    console.log(target)
 }
