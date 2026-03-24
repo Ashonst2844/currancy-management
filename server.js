ad55d76cfe47a614a6013a746f384a651773f4ef
Masagus Ahmad Ramadhan <agusyantosugiyanto@gmail.com>
2026-03-24 12:42:33 +0700

    massage

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// node server.js
+
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
@@ -5,19 +7,16 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-// 2. Middleware
-app.use(cors()); // Mengizinkan browser mengakses server ini
-app.use(express.json()); // Agar server bisa membaca data format JSON
+app.use(cors());
+app.use(express.json());
 
-// 3. Konfigurasi Koneksi MySQL
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',      // Username default XAMPP
-    password: '',      // Password default XAMPP (kosong)
-    database: 'currency-app' // GANTI dengan nama database Anda
+    user: 'root',
+    password: '',
+    database: 'currency-app'
 });
 
-// Koneksi ke Database
 db.connect((err) => {
     if (err) {
         console.error('Gagal koneksi ke MySQL:', err);
@@ -26,18 +25,15 @@ db.connect((err) => {
     console.log('Terhubung ke database MySQL!');
 });
 
-// 4. Membuat "Route" atau Alamat API
-// Ketika browser memanggil http://localhost:3000/api/currencies
 app.get('/api/currencies', (req, res) => {
-    const sql = "SELECT * FROM currencydb"; // GANTI dengan nama tabel Anda
+    const sql = "SELECT * FROM currencydb";
     
     db.query(sql, (err, results) => {
         if (err) return res.status(500).json({ error: err.message });
-        res.json(results); // Kirim data hasil query ke browser
+        res.json(results);
     });
 });
 
-// 5. Jalankan Server
 app.listen(port, () => {
     console.log(`Server berjalan di http://localhost:${port}`);
 });
